2728b2854e67c5b45a8ec92b29f0a5673fd2dd88
Erick Corona <www.coronas1@hotmail.com>
2025-10-22 11:49:11 -0600

    Se añadió la práctica 12

---
+++ Practica 13/Practica 13.py
@@ -0,0 +1,2 @@
+# Practica 13. Crear un programa que por medio de la camara detecte las manos y dibuje una calculadora en pantalla funcional
+# Tambien debe permitir operar con la calculadora usando las manos
